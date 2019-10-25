52e10e82f616264977868be076909c31d96b36a9
conranpearce <54678624+conranpearce@users.noreply.github.com>
2019-10-25 23:44:50 +0100

    Photo included
    Image of myself included

--- website-git.js
+++ website-git.js
@@ -1,3 +1,25 @@
-function test() {
-    console.log("Test");
+
+
+// Make image not dragable
+// Animate.css on image
+// Instagram feed
+// Video
+
+
+
+
+// function start() {
+
+//     var element = document.getElementById("portrait");
+
+//     element.classList.add('animated', 'bounceInUp');
+// }
+
+
+window.onload = function() {
+    var element = document.getElementById("portrait");
+
+    element.classList.add('animated', 'fadeIn');
 }
+// const element =  document.querySelector('.my-element')
+// element.classList.add('animated', 'bounceOutLeft')
