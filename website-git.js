472e989c21433ada91a15ee96700276fc438a9e9
conranpearce <54678624+conranpearce@users.noreply.github.com>
2019-10-26 11:34:46 +0100

    images upload

--- website-git.js
+++ website-git.js
@@ -4,8 +4,15 @@
 // Animate.css on image
 // Instagram feed
 // Video
-
-
+// Homepage - load image then scroll down to short about and links
+// Sort image by percentage
+// Link to linkedin and github - new tabs created
+// - Linked in and github become coloured when hovered
+// Tools used - greyscaled and go on colour when highlighted
+// Tools created a new tab to the website
+// Image slideshow of fighting images - karate and muay thai
+// Home button same price
+// Resize first photo
 
 
 // function start() {
@@ -18,8 +25,16 @@
 
 window.onload = function() {
     var element = document.getElementById("portrait");
-
     element.classList.add('animated', 'fadeIn');
+
+    // this.setTimeout(function() {
+    //     window.scrollTo(0,document.body.scrollHeight);
+
+    // },3000);
+}
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
 }
 // const element =  document.querySelector('.my-element')
 // element.classList.add('animated', 'bounceOutLeft')
