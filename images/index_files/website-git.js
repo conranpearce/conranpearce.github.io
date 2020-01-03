45f356089836e1e6441009c7ff9ff48ff961c1bc
conranpearce <54678624+conranpearce@users.noreply.github.com>
2020-01-03 15:16:38 +0000

    Changed front page

---
+++ images/index_files/website-git.js
@@ -0,0 +1,45 @@
+
+
+// Make image not dragable
+// Animate.css on image
+// Video
+// Homepage - load image then scroll down to short about and links
+// Sort image by percentage
+// Link to linkedin and github - new tabs created
+// - Linked in and github become coloured when hovered
+// Tools used - greyscaled and go on colour when highlighted
+// Tools created a new tab to the website
+// Image slideshow of fighting images - karate and muay thai
+// Home button same price
+// Resize first photo
+// About me link from homepage
+// Home page - photo on right text on left and links below
+// Photo page of my film photos?
+// Load photo in center then move to right and load text next to it
+// CV download from about page?
+// Make image clicable to about me page from homepage?
+// Lewis website text?
+
+// function start() {
+
+//     var element = document.getElementById("portrait");
+
+//     element.classList.add('animated', 'bounceInUp');
+// }
+
+
+window.onload = function() {
+    var element = document.getElementById("portrait");
+    // element.classList.add('animated', 'fadeIn');
+
+    // this.setTimeout(function() {
+    //     window.scrollTo(0,document.body.scrollHeight);
+
+    // },3000);
+}
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+// const element =  document.querySelector('.my-element')
+// element.classList.add('animated', 'bounceOutLeft')
