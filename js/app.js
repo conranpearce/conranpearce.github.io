97b9d38b99628365d9565c34945a4d49140a39a0
conranpearce <54678624+conranpearce@users.noreply.github.com>
2020-03-23 17:10:44 +0000

    removed images and added videos that play on hover

--- js/app.js
+++ js/app.js
@@ -11,3 +11,21 @@ window.onscroll = function () {
         this.nav.classList.add("header-nobg");
     }    
 };
+
+// JQUERY
+
+var figure = $(".video");
+var vid = $("video");
+
+[].forEach.call(figure, function (item) {
+        item.addEventListener('mouseover', hoverVideo, false);
+        item.addEventListener('mouseout', hideVideo, false);
+});
+
+function hoverVideo(e) {  
+        $(this).find('.thevideo')[0].play();
+}
+
+function hideVideo(e) {
+        $(this).find('.thevideo')[0].pause(); 
+}
